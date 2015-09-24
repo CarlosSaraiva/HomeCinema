9945b5ed20a6b7458e9bcd0b9da0a0ff7e046418
Carlos Saraiva <carlos.saraiva@msn.com>
2015-09-24 18:58:28 -0300

    Infraesctructure created.

---
+++ HomeCinema.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private HomeCinemaContext _dbContext;
+
+        public HomeCinemaContext Init()
+        {
+            return _dbContext ?? (_dbContext = new HomeCinemaContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            _dbContext?.Dispose(); ;
+        }
+    }
+}
---
+++ HomeCinema.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory _dbFactory;
+        private HomeCinemaContext _dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this._dbFactory = dbFactory;
+        }
+
+        //A Get method using lambda
+        public HomeCinemaContext DbContext => _dbContext ?? (_dbContext = _dbFactory.Init());
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
