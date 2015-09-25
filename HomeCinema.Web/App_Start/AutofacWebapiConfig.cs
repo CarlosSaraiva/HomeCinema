167ebbc5a861352adfcc3ca50a04f4c4a4d16e58
Carlos Saraiva <carlos.saraiva@msn.com>
2015-09-25 17:27:27 -0300

    WebApi starte.

---
+++ HomeCinema.Web/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,56 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using HomeCinema.Data;
+using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace HomeCinema.Web.App_Start
+{
+    public class AutofacWebapiConfig
+    {
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //EF HomeCinemaContext
+            builder.RegisterType<HomeCinemaContext>()
+                .As<IDbFactory>()
+                .InstancePerRequest();
+
+            builder.RegisterType<UnitOfWork>()
+                .As<UnitOfWork>()
+                .InstancePerRequest();
+
+            builder.RegisterGeneric(typeof(EntityBaseRepository<>))
+                .As(typeof(IEntityBaseRepository<>))
+                .InstancePerRequest();
+
+            //Services
+            builder.RegisterType<EncryptionService>()
+                .As<IEncryptionService>()
+                .InstancePerRequest();
+
+            Container = builder.Build();
+            return Container;
+        }
+    }
+}
