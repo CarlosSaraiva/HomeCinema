c7d52c0eeba1298649937c6741b8c0e27e9f8d40
Carlos Saraiva <carlos.saraiva@msn.com>
2015-09-25 18:18:47 -0300

    Update

---
+++ HomeCinema.Web/Infranstructure/Extensions/RequestMessageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using HomeCinema.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Dependencies;
+
+namespace HomeCinema.Web.Infranstructure.Extensions
+{
+    public static class RequestMessageExtensions
+    {
+        internal static IMembershipService GetMembershipService(this HttpRequestMessage request)
+        {
+            return request.GetService<IMembershipService>();
+        }
+
+        private static TService GetService<TService>(this HttpRequestMessage request)
+        {
+            IDependencyScope dependencyScope = request.GetDependencyScope();
+            TService service = (TService)dependencyScope.GetService(typeof(TService));
+            return service;
+        }
+    }
+}
