a580a89bf9462b30bb3a5d3dd26cd74964f4dd2a
Carlos Saraiva <carlos.saraiva@msn.com>
2015-09-25 18:05:41 -0300

    Implementing security.

---
+++ HomeCinema.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HomeCinema.Web.App_Start
+{
+    internal class Bootstrapper
+    {
+        public static void Run()
+        {
+            AutofacWebapiConfig.Initialize(GlobalConfiguration.Configuration);
+            //Configure automapper
+            //AutoMapperConfiguration.Configure();
+        }
+    }
+}
--- HomeCinema.Web/Global.asax.cs
+++ HomeCinema.Web/Global.asax.cs
@@ -1,23 +1,29 @@
-﻿using System;
+﻿using HomeCinema.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
-using System.Web.Http;
 
 namespace HomeCinema.Web
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        private void Application_Start(object sender, EventArgs e)
         {
+            var config = GlobalConfiguration.Configuration;
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            WebApiConfig.Register(config);
+            Bootstrapper.Run();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configuration.EnsureInitialized();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
