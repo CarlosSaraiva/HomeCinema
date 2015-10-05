996725278b2c28e71039257fcbb2e98f3d1cde09
Carlos Saraiva <carlos.saraiva@msn.com>
2015-10-05 15:40:22 -0300

    Controller Root criado

---
+++ HomeCinema.Web/Scripts/spa/home/rootCtrl.js
@@ -0,0 +1,18 @@
+﻿(function (app) {
+    'user strict';
+    app.controller('rootCtrl', rootCtrl);
+
+    function rootCtrl($scope) {
+        $scope.UserData = {};
+        $scope.UserData.displayUserInfo = displayUserInfo;
+        $scope.logout = logout;
+    }
+
+    function displayUserInfo() {
+    }
+
+    function logout() {
+    }
+
+    $scope.UserData.displayUserInfo();
+})(angular.module('homeCinema'));
